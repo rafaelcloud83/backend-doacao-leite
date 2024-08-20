157fecde654cbba6eab39b74ea439f549635f962
rafaelcloud83 <rafaelcloud83@gmail.com>
2024-08-20 09:48:50 -0300

    first commit

---
+++ src/main/java/edu/rafael/doacao_leite/DoacaoLeiteApplication.java
@@ -0,0 +1,13 @@
+package edu.rafael.doacao_leite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoacaoLeiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoacaoLeiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/rafael/doacao_leite/DoacaoLeiteApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rafael.doacao_leite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoacaoLeiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
